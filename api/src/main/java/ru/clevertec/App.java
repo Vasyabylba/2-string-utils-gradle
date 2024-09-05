5dbb7c2d07848d43cc0c805b8141c3c635cb1f45
Vasiliy Yalovchik <vasyabylba@gmail.com>
2024-09-05 21:18:41 +0300

    implement api module

---
+++ api/src/main/java/ru/clevertec/App.java
@@ -0,0 +1,9 @@
+package ru.clevertec;
+
+import util.Utils;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(Utils.isAllPositiveNumbers("12", "79"));
+    }
+}
